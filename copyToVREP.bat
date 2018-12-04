a41013e8ae0e23a207ace018d546b2044b235f05
BorisBogaerts <borisbogaerts@gmail.com>
2018-12-04 15:16:12 +0100

    Complete installer (+bat file)

---
+++ copyToVREP.bat
@@ -0,0 +1,28 @@
+echo off
+
+REM (this is a comment) Change the following string to your V-REP intallation location
+SET vrepLocation=C:\Program Files\V-REP3\V-REP_PRO_EDU
+REM Do not modify anything after this comment-
+SET luaLocation=%vrepLocation%
+SET modelLocation=%vrepLocation%\models
+SET sceneLocation=%vrepLocation%\scenes
+ 
+SET nexModel=%modelLocation%\VR-Interface
+SET nextScene=%sceneLocation%\VR-Interface
+
+SET current=%~dp0
+SET luaSource=%current%Scripts
+SET modelSource=%current%Models
+SET sceneSource=%current%Scene
+echo %current%
+echo %luaSource%
+echo %modelSource%
+echo %sceneSource%
+
+mkdir "%nextScene%"
+robocopy "%sceneSource%" "%nextScene%" /COPYALL /E
+
+mkdir "%nexModel%"
+robocopy "%modelSource%" "%nexModel%" /COPYALL /E
+
+robocopy "%luaSource%" "%luaLocation%" /COPYALL /E
